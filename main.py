fd5a94d7259b8b3d62d4ab4810bb6edef6cdfa99
dio <61555147+IThinkImOKAY@users.noreply.github.com>
2020-12-15 21:19:54 -0600

    Update main.py
    Fixed Syntax Error + List Index/Range Error

--- main.py
+++ main.py
@@ -11,22 +11,19 @@ client = pytumblr.TumblrRestClient(
 tags = ['your',
         'tags',
         'to',
-        'follow',
-        'and',
-        'like']
+        'like',
+        'and'
+        'follow']
 
 while True:
     try:
         for tag in tags:
             for item in client.tagged(tag):
                 if item["id"] is not None:
-                    client.like(item["id"] + item["reblog_key"])
+                    client.like(item["id"],item["reblog_key"])
                 if item["blog_name"] is not None:
                     client.follow(item["blog_name"] + ".tumblr.com")
-                print("Liked/Followed:",item["blog_name"])
+                print("Liked/Followed: ",item["blog_name"])
     except Exception:
         print(traceback.format_exc())
         time.sleep(60)
-    except KeyboardInterrupt:
-        print('shutting down :(')
-        quit()
